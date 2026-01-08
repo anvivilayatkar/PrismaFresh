4338c606507874ea1ae3e20140eb2df1f839cc4c
anvivilayatkar <anvivilayatkar@gmail.com>
2026-01-08 23:46:16 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -11,9 +11,4 @@ export interface CartItem extends Flower {
   quantity: number;
 }
 
-export interface ChatMessage {
-  role: 'user' | 'model';
-  text: string;
-}
-
 export type PageView = 'home' | 'shop' | 'contact' | 'order';
