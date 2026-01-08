e18044dbe40de112daf6771001bc70507f5ef4df
anvivilayatkar <anvivilayatkar@gmail.com>
2026-01-08 23:47:51 +0530

    Update imageAssets.ts

--- imageAssets.ts
+++ imageAssets.ts
@@ -1,16 +1,16 @@
 export const ASSETS = {
   // Site UI Images
-  HERO_BANNER: 'herobanner.png',
-  PHILOSOPHY_ART: 'philosophy_art.png',
-  STUDIO_INTERIOR: 'studio_interior.png',
+  HERO_BANNER: './herobanner.png',
+  PHILOSOPHY_ART: './philosophy_art.png',
+  STUDIO_INTERIOR: './studio_interior.png',
 
   // Product Images
-  PRODUCT_MARIGOLD: 'product_marigold.png',
-  PRODUCT_ROSE: 'product_rose.png',
-  PRODUCT_MOGRA: 'product_mogra.png',
-  PRODUCT_LOTUS: 'product_lotus.png',
-  PRODUCT_TUBEROSE: 'product_tuberose.png',
-  PRODUCT_HIBISCUS: 'product_hibiscus.png',
-  PRODUCT_ORCHID: 'product_orchid.png',
-  PRODUCT_MIX: 'product_mix.png',
+  PRODUCT_MARIGOLD: './product_marigold.png',
+  PRODUCT_ROSE: './product_rose.png',
+  PRODUCT_MOGRA: './product_mogra.png',
+  PRODUCT_LOTUS: './product_lotus.png',
+  PRODUCT_TUBEROSE: './product_tuberose.png',
+  PRODUCT_HIBISCUS: './product_hibiscus.png',
+  PRODUCT_ORCHID: './product_orchid.png',
+  PRODUCT_MIX: './product_mix.png',
 };
